96c0ee9ae087e2318199a54032c7d265de1ad108
permlap-phola <permlap@tatugacamp.com>
2024-05-06 22:40:48 +0700

    Improve Invention NRRU

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,11 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./index.html",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/primereact/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
